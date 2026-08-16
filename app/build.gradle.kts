25fc394d0c1a950534edbbbf6902e445fe3b2a50
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2026-08-16 15:43:29 +0530

    deps: add datastore-preferences, material-icons-extended, bundle warden fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.compose.ui)
     implementation(libs.compose.material3)
     implementation(libs.compose.foundation)
+    implementation(libs.compose.material.icons.extended)
     implementation(libs.activity.compose)
     implementation(libs.lifecycle.viewmodel.compose)
 }
