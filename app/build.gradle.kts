8e4953bcbc7f9c976940a8e8b92939b10e5a9e2e
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2026-09-14 01:10:17 +0530

    chore: bump version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.noxos.app"
         minSdk = 33
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.1.0"
     }
 
     buildTypes {
