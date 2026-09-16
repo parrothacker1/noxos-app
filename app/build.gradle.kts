f3ccd7a468a561264a2b636aa1e6f435f828512b
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2026-09-16 07:41:44 +0530

    chore: bump version to 2.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.noxos.app"
         minSdk = 33
         targetSdk = 35
-        versionCode = 8
-        versionName = "2.2.4"
+        versionCode = 9
+        versionName = "2.2.5"
     }
 
     signingConfigs {
