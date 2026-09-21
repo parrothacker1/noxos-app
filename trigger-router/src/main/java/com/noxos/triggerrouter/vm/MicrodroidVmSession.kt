af9a1e817da05ab6fa5b6fe911bc2968432e55c6
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2026-09-22 01:53:23 +0530

    chore: remove the DEBUG_LEVEL_FULL diagnostic flag now that the exit=1 investigation is closed

--- trigger-router/src/main/java/com/noxos/triggerrouter/vm/MicrodroidVmSession.kt
+++ trigger-router/src/main/java/com/noxos/triggerrouter/vm/MicrodroidVmSession.kt
@@ -42,7 +42,6 @@ class MicrodroidVmSessionFactory : VmSessionFactory {
         val config = VirtualMachineConfig.Builder(context)
             .setPayloadBinaryName("libnoxos_payload_stub.so")
             .setProtectedVm(false)
-            .setDebugLevel(VirtualMachineConfig.DEBUG_LEVEL_FULL)
             .build()
 
         val vmName = "noxos-scan-${UUID.randomUUID()}"
